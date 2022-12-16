babe57a7293f27a3a8a81cf87bfcaa901f76d2bd
Chika_Okafor <chikamichaelokafor@gmail.com>
2022-12-16 01:43:24 +0000

    added base home page tsx and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
+import { GitHub, Instagram, LinkedIn, Menu } from "@mui/icons-material";
 import React from "react";
-import "./App.css";
+import { Link, NavLink } from "react-router-dom";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span id="logo">CHIKA OKAFOR</span>
+        <Menu id="nav-btn" />
+        {/* <nav id="nav-menu">
+          <span className="nav-item">ABOUT</span>
+          <span className="nav-item">WORK</span>
+          <span className="nav-item">PORTFOLIO</span>
+        </nav> */}
       </header>
+      <main className="App-main">
+        <div id="hero">
+          <div className="hero-text">
+            FOR THE <br />
+            LOVE OF <br />
+            ALL THINGS <br />
+            FRONTEND <br />
+            DEVELOPMENT
+          </div>
+          <button className="call-to-action">explore my work</button>
+        </div>
+      </main>
+      <footer className="App-footer">
+        <span className="socials">
+          <Instagram className="social-icon" />
+          <LinkedIn className="social-icon" />
+          <GitHub className="social-icon" />
+        </span>
+        <span className="copyright">© 2022 CHIKA OKAFOR</span>
+      </footer>
     </div>
   );
 }
