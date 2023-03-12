c3a8c44d497ee703ead75e9e0cb208f869d5899c
Chika_Okafor <chikamichaelokafor@gmail.com>
2023-03-12 14:48:18 +0000

    fix: lint changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,20 +4,6 @@ import Hero from "../components/hero/Hero";
 import "./Home";
 
 const Home = () => {
-  const hero = (
-    <section className="hero-section">
-      <div className="hero-text">
-        Well, aren't you just the luckiest?
-        <br />
-        Prepare to be dazzled by my mediocre front-end development skills and
-        underwhelming design choices.
-      </div>
-      <button data-testid="call-to-action" className="call-to-action">
-        start here
-      </button>
-    </section>
-  );
-
   return <PageCreator id="hero" dataTestID="hero" grandChildren={<Hero />} />;
 };
 
