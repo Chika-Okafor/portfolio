b7e10439ad830f67b56940d94f9c434a66a1daac
Chika Okafor <55348966+Chika-Okafor@users.noreply.github.com>
2024-03-21 22:22:25 +0000

    chore: gh-pages setup (#1)

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,8 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(scss|sass|css)$": "identity-obj-proxy",
+  },
   // Other Jest configuration options...
 };
